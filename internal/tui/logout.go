c1af0eb418cde6c0cb2287523afd0336d6d29f5b
Matt Wang <ymattw@users.noreply.github.com>
2025-06-26 19:52:14 +0200

    Implement logout functionality

--- internal/tui/logout.go
+++ internal/tui/logout.go
@@ -1,12 +1,24 @@
 package tui
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/ymattw/tenuki/internal/config"
+)
+
 func logoutFunc(app *App) func() {
 	return func() {
-		app.popUp("Logout? Username/password needed next time.",
+		app.popUp(fmt.Sprintf("Logout %s? Client ID/secret and username/password needed next time.", app.client.Username),
 			[]string{"Logout", "Quit instead", "Cancel"},
 			map[string]func(){
-				// TODO: implement logout
-				"Quit instead": func() { app.tui.Stop() },
+				"Logout": func() {
+					os.Remove(config.SecretPath(app.client.Username))
+					app.tui.Stop()
+				},
+				"Quit instead": func() {
+					app.tui.Stop()
+				},
 			})
 	}
 }
